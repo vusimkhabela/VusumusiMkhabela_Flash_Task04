116625205adebde35417c2a0886999d568de40a1
v.mkhabela <v.mkhabela@fdmgroup.com>
2024-12-19 16:18:00 +0200

    TC01 Working - minor code errors

--- VusumusiMkhabela_Flash_Task4/src/test/java/za/co/flash/application/pageObjectModel/RegisterPage.java
+++ VusumusiMkhabela_Flash_Task4/src/test/java/za/co/flash/application/pageObjectModel/RegisterPage.java
@@ -3,40 +3,95 @@ package za.co.flash.application.pageObjectModel;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class RegisterPage {
 	
 	WebDriver driver;
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	/*WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));
-	WebElement sendk = driver.findElement(By.tagName("h1"));*/
 	
-	public RegisterPage(WebDriver driver) {
-		this.driver = driver;
+	public RegisterPage() {
 	}
-	
-	public void registerUser(String fName, String lName, String gender, String emailAdd, String pass) {
-		//Validate Registration page
-		//Fill In Details
-		//Validate Register button
-		//Click Register button
-	}
-	
-	
 
+	public static WebElement registerOption (WebDriver driver) {
+		return driver.findElement(By.xpath("//a[contains(text(),'Register')]"));
+	}
+	
+	public static WebElement registerHeadingEl (WebDriver driver) {
+		return driver.findElement(By.tagName("h1"));
+	}
+	
+	public static WebElement registerFormEl (WebDriver driver) {
+		return driver.findElement(By.cssSelector("form[action='/register']"));
+	}
+	
+	public static WebElement genderEl(WebDriver driver)
+	{
+		return driver.findElement(By.xpath("//input[@id='gender-male']"));
+	}
+	
+	public static WebElement fNameEl(WebDriver driver)
+	{
+		return driver.findElement(By.xpath("//input[@id='FirstName']"));
+	}
+	
+	public static WebElement lNameEl (WebDriver driver) {
+		return driver.findElement(By.xpath("//input[@id='LastName']"));
+	}
+	
+	public static WebElement emailAddEl (WebDriver driver) {
+		return driver.findElement(By.xpath("//input[@id='Email']"));
+	}
+	
+	public static WebElement passEl (WebDriver driver) {
+		return driver.findElement(By.xpath("//input[@id='Password']"));
+	}
+	
+	public static WebElement confirmPassEl (WebDriver driver) {
+		return driver.findElement(By.xpath("//input[@id='ConfirmPassword']"));
+	}
+	
+	public static WebElement registerBtnEl (WebDriver driver) {
+		return driver.findElement(By.cssSelector("#register-button"));
+	}
+	
+	public void registerUser(String fName, String lName, String emailAdd, String pass) {
+		
+		System.out.println("####Test Case 01 done by V.Mkhabela#####");
+		System.out.println("\n\n--------------------Test Steps------------------------");
+		
+		//Step1: Click the Contact Link
+		registerOption(driver).click();
+		System.out.println("\n1. The user clicks the REGISTER link in the Navigation Bar.");
+		
+		//Step2: Validate Registration Form
+		Assert.assertTrue(registerFormEl(driver).isDisplayed());
+		System.out.println("1.1 The user is directed to the registration form.");
+		
+		//TestfromBrowser, ExpectedText
+		Assert.assertEquals(registerHeadingEl(driver).getText(), "Register");
+		System.out.println("1.2 The heading is correct: Register.");
+		
+		//Step 3: Fill in Registration Form
+		genderEl(driver).click();
+		System.out.println("1.3. The user selects MALE from the gender radio buttons.");
+		
+		fNameEl(driver).sendKeys(fName);
+		System.out.println("1.4. The user enters first name into the first name field.");
+		
+		lNameEl(driver).sendKeys(lName);
+		System.out.println("1.5. The user enters last name into the last name field.");
+		
+		emailAddEl(driver).sendKeys(emailAdd);
+		System.out.println("1.6. The user enters email into the email field.");
+		
+		passEl(driver).sendKeys(pass);
+		System.out.println("1.8. The user enters password Test into the password field.");
+		
+		confirmPassEl(driver).sendKeys(pass);
+		System.out.println("1.9. The user confirms the password into the confirm password field.");
+		
+		//Step 3: Register user
+		registerBtnEl(driver).click();
+		System.out.println("1.10 The user clicks the Register button.");
+	}
 }
--- VusumusiMkhabela_Flash_Task4/src/test/java/za/co/flash/application/testScripts/TC01HomeLanding.java
+++ VusumusiMkhabela_Flash_Task4/src/test/java/za/co/flash/application/testScripts/TC01HomeLanding.java
@@ -1,32 +1,39 @@
 package za.co.flash.application.testScripts;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import za.co.flash.application.pageObjectModel.HomeLandingPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import za.co.flash.application.pageObjectModel.RegisterPage;
 
 public class TC01HomeLanding {
 	
 	WebDriver driver;
-	HomeLandingPage home = new HomeLandingPage(driver);
-	RegisterPage register = new RegisterPage(driver);
+	
 	
 	@BeforeTest
 	public void start() {
 		//refresh & initiate the driver
-		home.landingPage();
-		register.registerUser(null, null, null, null, null);
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.manage().window().maximize();
+		System.out.println("Moving out of BeforeTest");
 	}
 	
 	@Test
 	public void run() {
 		//execute test case 01
-		//Validate landing page
-		
-		
+		//Register User
+		System.out.println("Entering in Test");
+		RegisterPage register = new RegisterPage(driver);
+		register.registerUser("Vusumusi", "Mkhabela", "v@f.c", "1234567890");
 	}
 	
 	@AfterTest
